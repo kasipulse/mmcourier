22556d3cb43693db75155d91485f6ddc40395d20
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-05-18 14:59:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './AdminDashboard';
-// import DriverApp from './DriverApp'; // We will create this next
 
-// MM Courier Navy & White Global Styles
 const globalStyles = `
   body { margin: 0; font-family: 'Inter', sans-serif; background-color: #F4F7F9; }
   * { box-sizing: border-box; }
@@ -14,16 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <style>{globalStyles}</style>
+    <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
     <Router>
       <Routes>
-        {/* The main Logistics Control Center */}
         <Route path="/" element={<AdminDashboard />} />
-        
-        {/* Placeholder for future Driver Mobile View */}
         <Route path="/driver" element={<div>Driver Mobile View - Coming Soon</div>} />
-        
-        {/* Add more routes for Invoicing or Customer tracking here */}
       </Routes>
     </Router>
   </React.StrictMode>
